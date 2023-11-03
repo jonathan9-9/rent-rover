c83e09d681faa6b876152bce75b118b1da345e1b
Jonathan <jonathancornejox@gmail.com>
2023-11-02 23:40:30 -0700

    modal css and logic

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Nunito } from "next/font/google";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <Modal title="Hi, Welcome!" isOpen={true} />
           <Navbar />
         </ClientOnly>
         {children}
