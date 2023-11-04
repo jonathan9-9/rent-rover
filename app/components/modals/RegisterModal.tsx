1f15b9f3768b4f5d1b0667408a93ae4760c259be
Jonathan <jonathancornejox@gmail.com>
2023-11-03 21:24:11 -0700

    created register modal hook, axios post request of form data

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { AiFillGithub } from "react-icons/ai";
+import axios from "axios";
+import { useCallback, useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+
+const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    axios.post("/api/register", data);
+  };
+  return <div></div>;
+};
+
+export default RegisterModal;
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { Nunito } from "next/font/google";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
-import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal title="Hi, Welcome!" isOpen={true} />
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}
