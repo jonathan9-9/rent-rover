cba80413726daa02c06330532e8122bc8a4a565a
Jonathan <jonathancornejox@gmail.com>
2023-11-08 20:48:55 -0800

    used material UI - MUI to add click away component to have clickaway feature on user menu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
-import { useCallback, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import useLoginModal from "@/app/hooks/useLoginModal";
 import { User } from "@prisma/client";
 import { signOut } from "next-auth/react";
+import { ClickAwayListener } from "@mui/base/ClickAwayListener";
 
 interface UserMenuProps {
   currentUser?: User | null;
@@ -20,12 +21,18 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
   }, []);
+
+  const handleClickAway = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div className="relative">
-      <div className="flex flex-row gap-3 items-center">
-        <div
-          onClick={() => {}}
-          className="
+    <ClickAwayListener onClickAway={handleClickAway}>
+      <div className="relative">
+        <div className="flex flex-row gap-3 items-center">
+          <div
+            onClick={() => {}}
+            className="
         hidden
         md:block
         text-sm
@@ -37,12 +44,12 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
         transition
         cursor-pointer
         "
-        >
-          Airbnb your home
-        </div>
-        <div
-          onClick={toggleOpen}
-          className="
+          >
+            Airbnb your home
+          </div>
+          <div
+            onClick={toggleOpen}
+            className="
           p-4
           md: py-1
           md: px-2
@@ -57,16 +64,16 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
           hover: shadow-md
           transition
         "
-        >
-          <AiOutlineMenu />
-          <div className="hidden md:block">
-            <Avatar />
+          >
+            <AiOutlineMenu />
+            <div className="hidden md:block">
+              <Avatar />
+            </div>
           </div>
         </div>
-      </div>
-      {isOpen && (
-        <div
-          className="
+        {isOpen && (
+          <div
+            className="
         absolute
         rounded-xl
         shadow-md
@@ -78,27 +85,28 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
         top-12
         text-sm
         "
-        >
-          <div className="flex flex-col cursor-pointer">
-            {currentUser ? (
-              <>
-                <MenuItem onClick={() => {}} label="My reservations" />
-                <MenuItem onClick={() => {}} label="My favorites" />
-                <MenuItem onClick={() => {}} label="My trips" />
-                <MenuItem onClick={() => {}} label="My properties" />
-                <MenuItem onClick={() => {}} label="Airbnb my home" />
-                <MenuItem onClick={() => signOut()} label="Logout" />
-              </>
-            ) : (
-              <>
-                <MenuItem onClick={loginModal.onOpen} label="Login" />
-                <MenuItem onClick={registerModal.onOpen} label="Sign up" />
-              </>
-            )}
+          >
+            <div className="flex flex-col cursor-pointer">
+              {currentUser ? (
+                <>
+                  <MenuItem onClick={() => {}} label="My reservations" />
+                  <MenuItem onClick={() => {}} label="My favorites" />
+                  <MenuItem onClick={() => {}} label="My trips" />
+                  <MenuItem onClick={() => {}} label="My properties" />
+                  <MenuItem onClick={() => {}} label="Airbnb my home" />
+                  <MenuItem onClick={() => signOut()} label="Logout" />
+                </>
+              ) : (
+                <>
+                  <MenuItem onClick={loginModal.onOpen} label="Login" />
+                  <MenuItem onClick={registerModal.onOpen} label="Sign up" />
+                </>
+              )}
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </ClickAwayListener>
   );
 };
 
