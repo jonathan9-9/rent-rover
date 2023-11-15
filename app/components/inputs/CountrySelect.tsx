7fcc58d04de9271a6c1f603ee44f1df38606c246
Jonathan <jonathancornejox@gmail.com>
2023-11-14 16:30:42 -0800

    select border color change & on select value persistence

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -20,7 +20,35 @@ const CountrySelect: React.FC<CountrySelectProps> = ({ value, onChange }) => {
   const { getAll } = useCountries();
   return (
     <div>
-      <Select placeholder="Anywhere" isClearable options={getAll()} />
+      <Select
+        placeholder="Anywhere"
+        isClearable
+        options={getAll()}
+        onChange={(value) => onChange(value as CountrySelectValue)}
+        formatOptionLabel={(option: any) => (
+          <div className="flex flex-row items-center gap-3">
+            <div>{option.flag}</div>
+            <div>
+              {option.label},
+              <span className="text-neutral-500 ml-1">{option.region}</span>
+            </div>
+          </div>
+        )}
+        classNames={{
+          control: () => `p-2 border-2`,
+          input: () => "text-lg",
+          option: () => "text-lg",
+        }}
+        theme={(theme) => ({
+          ...theme,
+          borderRadius: 5,
+          colors: {
+            ...theme.colors,
+            primary: "black",
+            primary25: "#87ceeb",
+          },
+        })}
+      />
     </div>
   );
 };
