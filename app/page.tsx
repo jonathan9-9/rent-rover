011746bd17581bea93fca4835e84a543faa2fdec
Jonathan <jonathancornejox@gmail.com>
2023-11-17 14:13:31 -0800

    tailwind css for listings index page and reset filter logic via empty state component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+
 export default function Home() {
-  return <div className="text-rose-500 text-2xl">hi</div>;
+  const isEmpty = true;
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+  return (
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+          <div>My Listings</div>
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
