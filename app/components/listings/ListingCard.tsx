c3e5770dbda78f4d356a70750bc452a284aaa63a
Jonathan <jonathancornejox@gmail.com>
2023-11-19 14:18:26 -0800

    favorites api post and delete

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -9,6 +9,7 @@ import { format } from "date-fns";
 import Image from "next/image";
 import HeartButton from "../HeartButton";
 import { useMemo } from "react";
+import Button from "../Button";
 
 interface ListingCardProps {
   data: Listing;
@@ -72,12 +73,30 @@ const ListingCard: React.FC<ListingCardProps> = ({
             fill
             alt="Listing"
             src={data.imageSrc}
-            className="object-hover h-full w-full group-hover:scale-110 transition"
+            className="object-cover h-full w-full group-hover:scale-110 transition"
           />
           <div className="absolute top-3 right-3">
             <HeartButton listingId={data.id} currentUser={currentUser} />
           </div>
         </div>
+        <div className="font-semidbold text-lg">
+          {location?.region}, {location?.label}
+        </div>
+        <div className="font-light text-neutral-500">
+          {reservationDate || data.category}
+        </div>
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semibold">${price}</div>
+          {!reservation && <div className="font-light">night</div>}
+        </div>
+        {onAction && actionLabel && (
+          <Button
+            disabled={disabled}
+            small
+            label={actionLabel}
+            onClick={handleCancel}
+          />
+        )}
       </div>
     </div>
   );
