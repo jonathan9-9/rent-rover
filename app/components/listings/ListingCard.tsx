1caafaf378ce73e31d4e415c87578302d613c878
Jonathan <jonathancornejox@gmail.com>
2023-11-21 22:27:05 -0800

    trips router on reservation creation

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { SafeUser, SafeListing } from "@/app/types";
+import { SafeUser, SafeListing, SafeReservation } from "@/app/types";
 import { useRouter } from "next/navigation";
-import { Reservation } from "@prisma/client";
 import useCountries from "@/app/hooks/useCountries";
 import { useCallback } from "react";
 import { format } from "date-fns";
@@ -13,7 +12,7 @@ import Button from "../Button";
 
 interface ListingCardProps {
   data: SafeListing;
-  reservation?: Reservation;
+  reservation?: SafeReservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
   actionLabel?: string;
