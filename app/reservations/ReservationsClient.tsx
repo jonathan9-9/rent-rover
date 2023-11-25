f4be4129b366765342ba773b1607c4ce705eafec
Jonathan <jonathancornejox@gmail.com>
2023-11-25 14:19:02 -0800

    reservations client

--- app/reservations/ReservationsClient.tsx
+++ app/reservations/ReservationsClient.tsx
@@ -1,6 +1,16 @@
 "use client";
 
-const ReservationsClient = () => {
+import { SafeReservation } from "../types";
+
+interface ReservationsClientProps {
+  currentUser?: string;
+  reservations?: SafeReservation;
+}
+
+const ReservationsClient: React.FC<ReservationsClientProps> = ({
+  currentUser,
+  reservations,
+}) => {
   return <div>Reservations on my property</div>;
 };
 
