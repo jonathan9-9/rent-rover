4df391703e7415be7ae9f08a84065ce739ce8d33
Jonathan <jonathancornejox@gmail.com>
2023-11-25 15:37:23 -0800

    reservation iteration to place each one into a grid for list of reservation on client side

--- app/reservations/ReservationsClient.tsx
+++ app/reservations/ReservationsClient.tsx
@@ -1,17 +1,57 @@
 "use client";
-
-import { SafeReservation } from "../types";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useCallback, useState } from "react";
+import { SafeReservation, SafeUser } from "../types";
+import { useRouter } from "next/navigation";
+import Heading from "../components/Heading";
+import ListingCard from "../components/listings/ListingCard";
+import Container from "../components/Container";
 
 interface ReservationsClientProps {
-  currentUser?: string;
-  reservations?: SafeReservation;
+  currentUser?: SafeUser | null;
+  reservations?: SafeReservation[];
 }
 
 const ReservationsClient: React.FC<ReservationsClientProps> = ({
   currentUser,
   reservations,
 }) => {
-  return <div>Reservations on my property</div>;
+  const router = useRouter();
+  const [deletingId, setDeletingId] = useState("");
+
+  const onCancel = useCallback(
+    (id: string) => {
+      setDeletingId(id);
+      axios
+        .delete(`/api/reservations/${id}`)
+        .then(() => {
+          toast.success("Reservation cancelled successfully!");
+          router.refresh();
+        })
+        .catch(() => {
+          toast.error("Failed to cancel reservation");
+        })
+        .finally(() => {
+          setDeletingId("");
+        });
+    },
+    [router]
+  );
+  return (
+    <Container>
+      <Heading title="Reservations" subtitle="Bookings on your properties" />
+      <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
+        {reservations?.map((reservation) => (
+          <ListingCard
+            key={reservation.id}
+            data={reservation.listing}
+            reservation={reservation}
+          />
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 export default ReservationsClient;
