4ff0bbff7dc6f26dc57a7b29f970df6a6aea5700
Jonathan <jonathancornejox@gmail.com>
2023-12-02 21:26:19 -0800

    all other properties of getListing filtering options

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,17 +2,58 @@ import prisma from "@/app/libs/prismadb";
 
 export interface IListingParams {
   userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
 }
 
 export default async function getListings(params: IListingParams) {
   try {
-    const { userId } = params;
+    const {
+      userId,
+      guestCount,
+      roomCount,
+      bathroomCount,
+      startDate,
+      endDate,
+      locationValue,
+      category,
+    } = params;
 
     let query: any = {};
 
     if (userId) {
       query.userId = userId;
     }
+
+    if (category) {
+      query.category = category;
+    }
+
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount,
+      };
+    }
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount,
+      };
+    }
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount,
+      };
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
     const listings = await prisma.listing.findMany({
       where: query,
       orderBy: {
