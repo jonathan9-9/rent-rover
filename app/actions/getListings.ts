5d58131e1206a11ef181580061f8dc78ac650515
Jonathan <jonathancornejox@gmail.com>
2023-12-02 21:45:17 -0800

    getListings dateRange filter and category filter

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -56,7 +56,7 @@ export default async function getListings(params: IListingParams) {
 
     if (startDate && endDate) {
       query.NOT = {
-        reservations: {
+        reservation: {
           some: {
             OR: [
               {
