46cfe727a06f2c0f6cd4b801a3a3431dc9fab1ae
Jonathan <jonathancornejox@gmail.com>
2023-12-03 11:19:56 -0800

    grid spacing page tsx

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ const Home = async ({ searchParams }: HomeProps) => {
   return (
     <ClientOnly>
       <Container>
-        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+        <div className="pt-24 gap-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
           {listings.map((listing) => {
             return (
               <ListingCard
